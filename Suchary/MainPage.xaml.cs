204a206c32c182c4786645b9498ae10c29ecfdee
Michaldwadwa <michalskalkowski@gmail.com>
2014-06-19 13:49:23 +0200

    init

---
+++ Suchary/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace Suchary
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void pobierzSuchary()
+        {
+            WebClient webClient = new WebClient();
+            webClient.DownloadStringCompleted += new DownloadStringCompletedEventHandler(webClient_DownloadStringCompleted);
+            webClient.DownloadStringAsync(new Uri("http://kubavic.vdl.pl/suchary.json"));
+        }
+
+
+        void webClient_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            var rootObject = JsonConvert.DeserializeObject<RootObject>(e.Result);
+            foreach (var blog in rootObject.MyBlogList)
+            {
+                Console.WriteLine(blog.TITLE);
+            }
+        }
+
+
+
+    }
+}
---
+++ Suchary/Suchar.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System;
+
+
+public class Suchar
+{
+    public int ID { get; set; }
+    public string suchar { get; set; }
+}
+
+public class RootObject
+{
+    public List<Suchar> suchar { get; set; }
+}
